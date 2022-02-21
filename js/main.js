174ee026b4ae35ffcc677059307b90904f51ca60
mong0000 <99624171+mong0000@users.noreply.github.com>
2022-02-21 13:45:13 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    });
+
+$('.search_bar').hide();
+
+$('.search_btn').on('click',function(){
+    $('.search_bar').toggle();
+});
